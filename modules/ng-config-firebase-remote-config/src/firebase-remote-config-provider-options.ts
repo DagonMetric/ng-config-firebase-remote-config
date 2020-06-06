4e08a468163161ac865f39bd435b4171af977ec1
Moe Myat Zaw <mmzliveid@gmail.com>
2020-06-06 23:45:26 +0630

    Update FirebaseRemoteConfigProvider options

--- modules/ng-config-firebase-remote-config/src/firebase-remote-config-provider-options.ts
+++ modules/ng-config-firebase-remote-config/src/firebase-remote-config-provider-options.ts
@@ -8,27 +8,36 @@
 
 import { InjectionToken } from '@angular/core';
 
-import { FirebaseConfig } from './firebase-config';
+export interface FirebaseConfig {
+    apiKey?: string;
+    projectId?: string;
+    appId?: string;
+    authDomain?: string;
+    databaseURL?: string;
+    storageBucket?: string;
+    messagingSenderId?: string;
+    [key: string]: string | undefined;
+}
 
 export interface FirebaseRemoteConfigSettings {
     /**
      * Defines the maximum age in milliseconds of an entry in the config cache before
      * it is considered stale. Defaults to 43200000 (Twelve hours).
      */
-    minimumFetchIntervalMillis: number;
+    minimumFetchIntervalMillis?: number;
 
     /**
      * Defines the maximum amount of milliseconds to wait for a response when fetching
      * configuration from the Remote Config server. Defaults to 60000 (One minute).
      */
-    fetchTimeoutMillis: number;
+    fetchTimeoutMillis?: number;
 }
 
 export interface FirebaseRemoteConfigProviderOptions {
     firebaseConfig: FirebaseConfig;
     appName?: string;
     remoteConfigSettings?: FirebaseRemoteConfigSettings;
-    throwFetchError?: boolean;
+    throwIfLoadError?: boolean;
 }
 
 export const FIREBASE_REMOTE_CONFIG_PROVIDER_OPTIONS = new InjectionToken<FirebaseRemoteConfigProviderOptions>(
