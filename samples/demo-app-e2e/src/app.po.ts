bd2faf92d100f816c77f32bd37a1845fbcaded7d
Moe Myat Zaw <mmzliveid@gmail.com>
2020-06-16 00:32:11 +0630

    Update demo app e2e

--- samples/demo-app-e2e/src/app.po.ts
+++ samples/demo-app-e2e/src/app.po.ts
@@ -8,4 +8,16 @@ export class AppPage {
     getTitleText(): Promise<string> {
         return element(by.css('app-root header h1')).getText() as Promise<string>;
     }
+
+    getMapTypeSectionText(): Promise<string> {
+        return element(by.css('app-root section .app-options-name')).getText() as Promise<string>;
+    }
+
+    getMapObjectSectionText(): Promise<string> {
+        return element(by.css('app-root section .child-options-key1')).getText() as Promise<string>;
+    }
+
+    getGetValueSectionText(): Promise<string> {
+        return element(by.css('app-root section .key1')).getText() as Promise<string>;
+    }
 }
